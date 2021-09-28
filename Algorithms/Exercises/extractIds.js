c0759a2d0fc737b4e0f96dccbc06481c12415f47
Tochukwu Ozurumba <ozurumbatochukwu@yahoo.com>
2021-09-28 16:21:42 +0100

    :star: extract ids

--- Algorithms/Exercises/extractIds.js
+++ Algorithms/Exercises/extractIds.js
@@ -1,19 +1,15 @@
-const str =  "*hjj [~accountid:612c978d1dbcd9006917329b] [~accountid:612c978d1dbcd9006917329b] Hiiiiii*";
+const str =  "[~accountid:612c978d1dbcd9006917329b] Hiiiiii [~accountid:612c978d1dbcd9006917329b]";
 
 const extractIds = (str) => {
-  const user = [];
-  let i = 0;
-  let P = 'accountid:'
-  const size = P.length;
-
-  while (i < str.length) {
-    let sizeCheck = str.slice(i, size + i);
-    if (sizeCheck === P) {
-      user.push(str.slice(size + i, 24 + size+ i));
-    }
-    i++;
-  }
- return user;
+  return [... new Set(str
+    .replace(/\~accountid:/gm, "") // extract [id]
+    // .replace(/ +/gm, "") // 2 spaces to 1
+    .match(/(\[(.*?)])/gm))]
+    .map((item) => {
+      return item
+        .replace('[', '')
+        .replace(']', '')
+    });
 }
 
 console.log(extractIds(str));
